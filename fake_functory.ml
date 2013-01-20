781428f56976692939ad6bf457c660a9258775b1
Alain Mebsout <alainmebsout@gmail.com>
2013-01-20 19:52:06 +0000

    headers 2013

--- fake_functory.ml
+++ fake_functory.ml
@@ -1,3 +1,17 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                              Cubicle                                   *)
+(*                                                                        *)
+(*                       Copyright (C) 2011-2013                          *)
+(*                                                                        *)
+(*                  Sylvain Conchon and Alain Mebsout                     *)
+(*                       Universite Paris-Sud 11                          *)
+(*                                                                        *)
+(*                                                                        *)
+(*  This file is distributed under the terms of the Apache Software       *)
+(*  License version 2.0                                                   *)
+(*                                                                        *)
+(**************************************************************************)
 
 
 module Cores = struct
