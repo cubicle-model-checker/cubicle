85f9c3c0cb43ec9bf9403c574c539e56056b4ea9
Alain Mebsout <alainmebsout@gmail.com>
2014-04-21 09:41:19 +0000

    oubli oracle

---
+++ oracle.mli
@@ -0,0 +1,21 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                              Cubicle                                   *)
+(*                                                                        *)
+(*                       Copyright (C) 2011-2013                          *)
+(*                                                                        *)
+(*                  Sylvain Conchon and Alain Mebsout                     *)
+(*                       Universite Paris-Sud 11                          *)
+(*                                                                        *)
+(*                                                                        *)
+(*  This file is distributed under the terms of the Apache Software       *)
+(*  License version 2.0                                                   *)
+(*                                                                        *)
+(**************************************************************************)
+
+module type S = sig
+
+    val init : Ast.t_system -> unit
+
+    val first_good_candidate : Node.t list -> Node.t option 
+end
