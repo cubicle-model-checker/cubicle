0f57e513b5c1b59160aec11351969b833347617a
Mattias Roux <Mattias.Roux@lri.fr>
2016-08-25 21:32:41 +0000

    arithmetique

--- approx.ml
+++ approx.ml
@@ -217,16 +217,16 @@ let cube_known_bad c =
 (*****************************************)
 
 let approx_arith a = match a with
-  | Atom.Comp (t, Eq, Const c) ->
-     begin
-       match const_sign c with
-       | None | Some 0 -> a
-       | Some n ->
-	  let zer = Const (add_constants c (mult_const (-1) c)) in
-	  if n < 0 then Atom.Comp (t, Lt, zer)
-	  else Atom.Comp (zer, Lt, t)
-     end
-  | _ -> a
+    | Atom.Comp (t, Eq, Const c) ->
+        begin
+          match const_sign c with
+            | None | Some 0 -> a
+            | Some n ->
+	        let zer = Const (add_constants c (mult_const (-1) c)) in
+	        if n < 0 then Atom.Comp (t, Lt, zer)
+	        else Atom.Comp (zer, Lt, t)
+        end
+    | _ -> a
 
 let approximations s =
   let args, sa = Node.variables s, Node.litterals s in
